2b352b52d952e30fdf0ec0b4fb130a1d1d0e7582
Mari <marur00911@gmail.com>
2025-11-20 12:14:00 -0300

    Agregar función 'crear_curso' para permitir la creación de nuevos cursos desde un formulario

--- curso/views.py
+++ curso/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Curso
 
 def listar_cursos(request):
     cursos = Curso.objects.all()
     return render(request, 'curso/listar_curso.html', {'cursos': cursos})
+
+def crear_curso(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        idioma = request.POST['idioma']
+        nivel = request.POST['nivel']
+        profesor = request.POST['profesor']
+        imagen = request.FILES.get('imagen')
+        
+        Curso.objects.create(
+            nombre=nombre,
+            idioma=idioma,
+            nivel=nivel,
+            profesor=profesor,
+            imagen=imagen
+        )
+        
+        return redirect('listar_cursos')
+    
+    return render(request, 'curso/crear_curso.html')
