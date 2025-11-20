5f2a19914c1f843b4f9950315b20285c1deb53ad
Mari <marur00911@gmail.com>
2025-11-20 14:04:30 -0300

    Agregar plantilla 'crear_curso.html' y actualizar 'listar_curso.html' para enlazar a la creación de nuevos cursos

--- curso/urls.py
+++ curso/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import listar_cursos
+from .views import listar_cursos, crear_curso
 
 urlpatterns = [
     path('listar/', listar_cursos, name='listar_cursos'),
+    path('crear/', crear_curso, name='crear_curso'),
 ]
