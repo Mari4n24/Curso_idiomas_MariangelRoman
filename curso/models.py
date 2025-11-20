06012ad0371bc4a199a84ab2c4e9baca50015315
Mari <marur00911@gmail.com>
2025-11-20 14:38:41 -0300

    Agregar campo 'idioma' al modelo 'Curso', actualizar formularios y vistas para reflejar cambios

--- curso/models.py
+++ curso/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Curso(models.Model):
-    nombre = models.CharField(max_length=30)
     nivel = models.CharField(max_length=20)
+    idioma = models.CharField(max_length=20)
     imagen = models.ImageField(upload_to='cursos', null=True, blank=True)
--- curso/views.py
+++ curso/views.py
@@ -5,20 +5,19 @@ def listar_cursos(request):
     cursos = Curso.objects.all()
     return render(request, 'curso/listar_curso.html', {'cursos': cursos})
 
+
 def crear_curso(request):
     if request.method == 'POST':
-        nombre = request.POST['nombre']
-        idioma = request.POST['idioma']
-        nivel = request.POST['nivel']
+        idioma = request.POST.get('idioma')
+        nivel = request.POST.get('nivel')
         imagen = request.FILES.get('imagen')
-        
+
         Curso.objects.create(
-            nombre=nombre,
             idioma=idioma,
             nivel=nivel,
             imagen=imagen
         )
-        
+
         return redirect('listar_cursos')
-    
+
     return render(request, 'curso/crear_curso.html')
