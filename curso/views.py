1abd749a806ea206b0c3191c6a3b36db15ac2f79
Mari <marur00911@gmail.com>
2025-11-24 17:21:10 -0300

    agregar las vistas de alumno y profesor

--- curso/views.py
+++ curso/views.py
@@ -3,29 +3,32 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, UpdateView, DeleteView
-from .models import Curso
+from .models import Curso, Alumno, Profesor
+from .forms import CursoForm, AlumnoForm, ProfesorForm
+
+# Cursos
 
 @login_required
 def listar_cursos(request):
-    cursos = Curso.objects.all()
-    return render(request, 'curso/listar_curso.html', {'cursos': cursos})
+    query = request.GET.get('q')
+    if query:
+        cursos = Curso.objects.filter(idioma__icontains=query)
+        mensaje = 'o se encontraron cursos' if not cursos else ''
+    else:
+        cursos = Curso.objects.all()
+        mensaje = 'No hay cursos cargados' if not cursos else ''
+    return render(request, 'curso/listar_curso.html', {'cursos': cursos, 'mensaje': mensaje})
 
 @login_required 
 def crear_curso(request):
     if request.method == 'POST':
-        idioma = request.POST.get('idioma')
-        nivel = request.POST.get('nivel')
-        imagen = request.FILES.get('imagen')
-
-        Curso.objects.create(
-            idioma=idioma,
-            nivel=nivel,
-            imagen=imagen
-        )
-
-        return redirect('listar_cursos')
-
-    return render(request, 'curso/crear_curso.html')
+        form = CursoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_cursos')
+    else:
+        form = CursoForm()
+    return render(request, 'curso/crear_curso.html', {'form': form})
 
 class CursoDetalleView(LoginRequiredMixin,DetailView):
     model = Curso
@@ -35,6 +38,7 @@ class CursoDetalleView(LoginRequiredMixin,DetailView):
 
 class CursoEditarView(LoginRequiredMixin, UpdateView):
     model = Curso
+    form_class = CursoForm
     fields = ['idioma', 'nivel', 'imagen']
     template_name = 'curso/editar_curso.html'
     success_url = reverse_lazy('listar_cursos')
@@ -43,3 +47,40 @@ class CursoBorrarView(LoginRequiredMixin, DeleteView):
     model = Curso
     template_name = 'curso/borrar_curso.html'
     success_url = reverse_lazy('listar_cursos')
+
+# Alumnos
+
+@login_required
+def listar_alumnos(request):
+    alumnos = Alumno.objects.all()
+    mensaje = 'No hay alumnos cargados' if not alumnos else ''
+    return render(request, 'curso/listar_alumno.html', {'alumnos': alumnos, 'mensaje': mensaje})
+
+@login_required
+def crear_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_alumnos')
+    else:
+        form = AlumnoForm()
+    return render(request, 'curso/crear_alumno.html', {'form': form})
+
+# Profesores
+@login_required
+def listar_profesores(request):
+    profesores = Profesor.objects.all()
+    mensaje = 'No hay profesores cargados' if not profesores else ''
+    return render(request, 'curso/listar_profesor.html', {'profesores': profesores, 'mensaje': mensaje})    
+
+@login_required
+def crear_profesor(request):
+    if request.method == 'POST':
+        form = ProfesorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_profesores')
+    else:
+        form = ProfesorForm()
+    return render(request, 'curso/crear_profesor.html', {'form': form})
