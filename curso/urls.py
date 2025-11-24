b55e3fefc406f763628a64d114a51ad4a5199153
Mari <marur00911@gmail.com>
2025-11-24 17:23:21 -0300

    agregar las url de listar y crear alumnos y profesores

--- curso/urls.py
+++ curso/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import listar_cursos, crear_curso, CursoDetalleView, CursoEditarView, CursoBorrarView
+from .views import *
 
 urlpatterns = [
     path('listar/', listar_cursos, name='listar_cursos'),
@@ -7,4 +7,8 @@ urlpatterns = [
     path('<int:pk>/', CursoDetalleView.as_view(), name='detalle_curso'),
     path('<int:pk>/editar/', CursoEditarView.as_view(), name='editar_curso'),
     path('<int:pk>/borrar/', CursoBorrarView.as_view(), name='borrar_curso'),
+    path('alumnos/listar/', listar_alumnos, name='listar_alumnos'),
+    path('alumnos/crear/', crear_alumno, name='crear_alumno'),
+    path('profesores/listar/', listar_profesores, name='listar_profesores'),
+    path('profesores/crear/', crear_profesor, name='crear_profesor'),
 ]
